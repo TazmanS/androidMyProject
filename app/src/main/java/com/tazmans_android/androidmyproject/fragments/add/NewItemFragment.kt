1d92cdb71ebf458a911bc55b45762de62ef76615
TazmanS <TazmanS123@gmail.com>
2023-03-22 19:54:01 +0200

    work at toolbar

--- app/src/main/java/com/tazmans_android/androidmyproject/fragments/add/NewItemFragment.kt
+++ app/src/main/java/com/tazmans_android/androidmyproject/fragments/add/NewItemFragment.kt
@@ -1,9 +1,9 @@
 package com.tazmans_android.androidmyproject.fragments.add
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import com.tazmans_android.androidmyproject.R
 import com.tazmans_android.androidmyproject.databinding.ActivityNewItemBinding
 import com.tazmans_android.androidmyproject.fragments.BaseFragment
 
@@ -16,6 +16,19 @@ class NewItemFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = ActivityNewItemBinding.inflate(inflater, container, false)
+
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.new_item_toolbar_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.v("MyTag", item.itemId.toString())
+
+        return super.onOptionsItemSelected(item)
     }
 }
